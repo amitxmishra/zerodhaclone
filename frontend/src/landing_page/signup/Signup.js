cfff158f913415f320f501e1476ae6fa9e5abb9c
delta student <mishraprasanta761@gmail.com>
2026-06-29 21:03:52 +0530

    Initial commit

---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-5">Open a free demat account</h1>
+        <p>Invest in everything</p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
